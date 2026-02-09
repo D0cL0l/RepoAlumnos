9ae570733e8a4d57e90d5a68af1fcde916061766
D0cL0l <99561054+D0cL0l@users.noreply.github.com>
2026-02-09 11:57:41 +0100

    Edad quitada

--- claseAlumnos.java
+++ claseAlumnos.java
@@ -2,11 +2,10 @@ import java.util.Random;
 
 public class claseAlumnos {
 	String nombre;
-	int telefono, edad, not1, not2, not3;
+	int telefono, not1, not2, not3;
 
-	public claseAlumnos(String nombre, int edad, int telefono) {
+	public claseAlumnos(String nombre, int telefono) {
 	this.nombre = nombre;
-	this.edad = edad;
 	this.telefono = telefono;
 	}
 	
