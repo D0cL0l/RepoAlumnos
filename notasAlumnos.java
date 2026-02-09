9ae3df5d047e0dc51b515dd7f81cc427287c3b1a
D0cL0l <99561054+D0cL0l@users.noreply.github.com>
2026-02-09 11:58:17 +0100

    Remove age input from notasAlumnos.java

--- notasAlumnos.java
+++ notasAlumnos.java
@@ -5,12 +5,11 @@ public class notasAlumnos {
 
 		Random rand = new Random();
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Dame un nombre, edad y telefono:");
+		System.out.println("Dame un nombre y telefono:");
 		String nombre = sc.nextLine();
-		int edad = sc.nextInt();
 		int telefono = sc.nextInt();
 
-		claseAlumnos alumno = new claseAlumnos(nombre, edad, telefono);
+		claseAlumnos alumno = new claseAlumnos(nombre, telefono);
 		alumno.Notas();	
 	}
 }
